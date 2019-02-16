57c4def32139d0f7b100251c0242912364188fd6
siegmann <siegmann@eomap.de>
2019-02-16 14:59:23 +0100

    - add geography

--- geodata/admin.py
+++ geodata/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.gis import admin
 from .models import *
 
+admin.site.register(Country, admin.GeoModelAdmin)
 admin.site.register(UserData, admin.GeoModelAdmin)
 admin.site.register(UserProjectShape, admin.GeoModelAdmin)
