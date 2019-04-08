9d11b9329f0de2cbc7cf70396c69c0400f404b7c
siegmann <siegmann@eomap.de>
2019-04-08 16:23:23 +0200

    - local time zone detection and recalcualte last_change for specific user

--- ews_db_connector/serializers.py
+++ ews_db_connector/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from projects.models import UserProject
 from .models import Mission
+import datetime
 
 
 class UserProjectSerializer(serializers.ModelSerializer):
@@ -11,8 +12,15 @@ class UserProjectSerializer(serializers.ModelSerializer):
 
 
 class MissionSerializer(serializers.ModelSerializer):
+    actiondatetime_local = serializers.SerializerMethodField()
+
+    def get_actiondatetime_local(self, obj):
+        local_time = int(self.context['request'].COOKIES['local_time'])
+        local_time *= -1
+        local_time = obj.actiondatetime + datetime.timedelta(minutes=local_time)
+        return local_time
 
     class Meta:
         model = Mission
         fields = ("ident", "get_scene_datetime", "state", "status_percentage", "actiondescription",
-                  "time_elapsed", "actiondatetime", "pk")
+                  "time_elapsed", "actiondatetime_local", "pk")
